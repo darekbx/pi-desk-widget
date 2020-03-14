63c8e6c33016607b4f29906b92d0fa48deed75db
DarekMac <darek.baranczuk@gmail.com>
2020-03-14 16:41:43 +0100

    Added chart and last cases diff

--- display.py
+++ display.py
@@ -5,15 +5,20 @@ import sys
 import time
 from PIL import Image, ImageDraw, ImageFont
 import traceback
+import math
+import epd2in7b
 
 from covid19 import Covid19
 
 class Display():
 
-    IS_DEBUG        = True
+    IS_DEBUG        = False
     display_width   = None
     display_height  = None
-    font_size       = 12
+    font_size       = 16
+    cache_file      = 'cache.dat'
+    cache_dir       = '/home/pi/display/'
+    font_path       = '/home/pi/pi-display/'
 
     def initialize(self):
         if self.IS_DEBUG:
@@ -30,10 +35,25 @@ class Display():
 
     def prepare_images(self):
 
-        data = Covid19().load()[::-1]
+        if self.IS_DEBUG:
+            data = [
+                { 'date': '2020-03-01', 'cases': 1},
+                { 'date': '2020-03-01', 'cases': 2},
+                { 'date': '2020-03-01', 'cases': 5},
+                { 'date': '2020-03-01', 'cases': 10},
+                { 'date': '2020-03-01', 'cases': 32},
+                { 'date': '2020-03-01', 'cases': 44},
+                { 'date': '2020-03-10', 'cases': 58},
+                { 'date': '2020-03-11', 'cases': 104}
+            ]
+        else:
+            data = Covid19().load()
 
         HBlackimage = self._draw_black(data)
         HRedimage = self._draw_red(data)
+
+        self._save_last(data[-1]['cases'])
+
         return HBlackimage, HRedimage
 
     def commit_to_display(self, HBlackimage, HRedimage):
@@ -62,30 +82,67 @@ class Display():
             self.display_height = epd2in7b.EPD_HEIGHT
 
     def _draw_red(self, data):
-        font = ImageFont.truetype('ObliviousFont.ttf', self.font_size)
+        font = ImageFont.truetype(self._font_path(), self.font_size)
         HRedimage = Image.new('1', (self.display_height, self.display_width), 255)
         drawred = ImageDraw.Draw(HRedimage)
 
-        y = 12
-        x = 150
-        for item in data:
-            drawred.text((x, y), "%s" % item['cases'], font=font, fill = 0)
-            y += 15
+        top_offset = 170
+        x = 6
+        step = math.floor((self.display_height - 6) / (len(data) - 1))
+        maximum = max(item['cases'] for item in data)
+        height_step = (self.display_width - 6 * 2) / maximum
+
+        last_x = 6
+        last_y = 0
+
+        for index, item in enumerate(data):
+            if index == 0:
+                last_y = int(item['cases'])
+                continue
+            
+            y1 = top_offset - last_y * height_step
+            y2 = top_offset - int(item['cases']) * height_step
+            drawred.line((x, y1, x + step, y2), fill = 0, width = 2)
+            x = x + step
+            last_y = int(item['cases'])
+        
+        last_value = int(self._load_last())
+        current_cases = int(data[-1]['cases'])
+        diff = current_cases - last_value
+
+        if diff > 0:
+            text = "{0} +{1}     ".format(current_cases, diff)
+        else:
+            text = "{0}    ".format(current_cases)
+        drawred.text((130, 6), text, font=font, fill = 0)
 
         return HRedimage
 
     def _draw_black(self, data):
-        font = ImageFont.truetype('ObliviousFont.ttf', self.font_size)
+        font = ImageFont.truetype(self._font_path(), self.font_size)
         HBlackimage = Image.new('1', (self.display_height, self.display_width), 255)
         drawblack = ImageDraw.Draw(HBlackimage)
 
-        y = 12
-        x = 10
-        for item in data:
-            drawblack.text((x, y), item['date'], font=font, fill = 0)
-            y += 15
+        drawblack.line((6, 6, 6, self.display_width - 6), fill = 0)
+        drawblack.line((6, self.display_width - 6, self.display_height - 6, self.display_width - 6), fill = 0)
+
+        drawblack.text((10, 6), "%s:    " % data[-1]['date'], font=font, fill = 0)
 
         return HBlackimage
+
+    def _save_last(self, value):
+        with open(self._cache_file(), 'w') as handle:
+            handle.write("%s" % value)
+
+    def _load_last(self):
+        with open(self._cache_file(), 'r') as handle:
+            return handle.readline()
+
+    def _cache_file(self):
+        return self.cache_file if self.IS_DEBUG else self.cache_dir + self.cache_file
+
+    def _font_path(self):
+        return 'theboldfont.ttf' if self.IS_DEBUG else '/home/pi/pi-display/theboldfont.ttf' 
 
 display = Display()
 display.initialize()
